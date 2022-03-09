b00c25a6cb7dedf20a05a3569fdcb6483083aa51
Fawad naeem <fawad4bros@gmail.com>
2022-03-09 19:48:17 +0500

    CRUD code refactor and bug free

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
 const { json } = require("body-parser");
 const config = require("dotenv");
 
@@ -7,15 +9,24 @@ const apis = require("./apis/api");
 const connectDB = require("./lib/config/db");
 
 const app = express();
-app.use(json()); //server can accept JSON data (req.body)
+app.use(json());
 app.use(apis);
 app.use("/image", express.static("./public/images"));
-// app.use(bodyParser.urlencoded({ extended: true }));
-
 config.config({ path: "./.env" });
 
+if (process.env.NODE_ENV === "development") {
+  morgan.token(
+    "timed",
+    "Method(:method) URL(:url) STATUS(:status) TIME (:total-time[2])"
+  );
+  let logStream = fs.createWriteStream(path.join(__dirname, "file.log"), {
+    flags: "a",
+  });
+  app.use(morgan("timed", { stream: logStream }));
+  app.use(morgan("timed"));
+}
 connectDB();
 const port = process.env.PORT || 7000;
 app.listen(port, () => {
-  console.log("Server Up");
+  console.log(`Server Up`);
 });
