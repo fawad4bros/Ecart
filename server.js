e0bffb75e3fdf0b810573662bdc03c41d76baff6
Fawad naeem <fawad4bros@gmail.com>
2022-03-14 22:49:59 +0500

    one more try

--- server.js
+++ server.js
@@ -4,23 +4,23 @@ const path = require("path");
 const { json } = require("body-parser");
 const dotenv = require("dotenv");
 
-const connectDB = require("./lib/config/db");
+const connectDB = require("./backend/lib/config/db");
 const app = express();
 
 app.use(json());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static("../frontend/dist/frontend"));
+app.use(express.static("/frontend/dist/frontend"));
 
-require("./app/product/routesDefinations/product.routeDefs")(app);
-require("./app/cart/routesDefinations/cart.routeDefs")(app);
-require("./app/user/routesDefinations/user.routeDefs")(app);
+require("./backend/app/product/routesDefinations/product.routeDefs")(app);
+require("./backend/app/cart/routesDefinations/cart.routeDefs")(app);
+require("./backend/app/user/routesDefinations/user.routeDefs")(app);
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: "./backend/.env" });
 
 connectDB();
 app.get("/*", (req, res) => {
   res.sendFile("index.html", {
-    root: "../frontend/dist/frontend",
+    root: "/frontend/dist/frontend",
   });
 });
 const port = process.env.PORT || 7000;
