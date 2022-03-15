86549a2cbb4511bcfa964f877b3199b3d4818024
Fawad naeem <fawad4bros@gmail.com>
2022-03-15 20:37:31 +0500

    sass to scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import{ PagesModule } from './pages/pages.module'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/constants/index.ts
@@ -0,0 +1,6 @@
+const baseUrl = "http://localhost:3000/api/product/";
+export class AppConfig {
+  public static size: number = 10;
+  public static page: number = 1;
+  public static URL_Product: string = baseUrl;
+}
